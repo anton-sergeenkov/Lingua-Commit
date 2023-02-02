ba68097b72e8d0c08777ee765f31624bd7ada547
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-02-02 15:31:14 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Lingua-Commit/',
+  base: '/lingua-commit/',
   plugins: [vue()],
   resolve: {
     alias: {
